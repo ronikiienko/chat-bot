81cf91122ea0f50737514612ac2ce9d07917c80a
Rostyslav Onikiienko <ronikiienko@gmail.com>
2023-02-06 18:26:43 +0200

    wip: make it answer on questions

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const {generateAnswer} = require('./generator');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
+bot.on('message', async (ctx) => {
+    const messageText = ctx.message.text;
+    const userId = ctx.message.chat.id;
+
+    const answer = await generateAnswer(messageText);
+    if (answer) ctx.sendMessage(answer);
+});
+
+bot.launch();
 
 let datastore = Datastore.create(path.join('db.db'));
 
